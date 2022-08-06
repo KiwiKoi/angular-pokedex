2f410065a4ca9dfe7de5a0fbbc296b1209fb4df1
Daniel <danielo.visage@gmail.com>
2022-08-06 21:03:24 +0200

    feat: add sprite to card

--- src/app/models/pokemon.ts
+++ src/app/models/pokemon.ts
@@ -1,6 +1,15 @@
 export interface Pokemon {
   id: number;
   name: string;
+  sprites: {
+    front_default: string;
+    front_shiny: string;
+    front_female: string;
+    front_shiny_female: string;
+    back_default: string;
+    back_female: string;
+    back_shiny_female: string;
+  };
   height: number;
   weight: number;
 }
