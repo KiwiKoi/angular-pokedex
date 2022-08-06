d04f5df518bd8d1ff3694968fb9542f76c5de56a
Daniel <danielo.visage@gmail.com>
2022-08-06 18:46:13 +0200

    fix : fix typeErrors

--- src/app/card-list/card-list.component.ts
+++ src/app/card-list/card-list.component.ts
@@ -1,28 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { Pokemon } from '../models/pokemon';
-import {PokeDataService} from '../services/poke-data.service';
+import { PokeDataService } from '../services/poke-data.service';
 
 @Component({
   selector: 'app-card-list',
   templateUrl: './card-list.component.html',
-  styleUrls: ['./card-list.component.scss']
+  styleUrls: ['./card-list.component.scss'],
 })
 export class CardListComponent implements OnInit {
-
   pokemon!: Pokemon;
   pokemonList!: Pokemon[];
 
-  constructor(private pokeDataService: PokeDataService){}
+  constructor(private pokeDataService: PokeDataService) {}
 
   ngOnInit(): void {
-  this.fetchAllPokemon();
+    this.fetchAllPokemon();
   }
 
-  fetchAllPokemon(){
+  fetchAllPokemon() {
     this.pokeDataService.getPokemonList().subscribe((response: Pokemon[]) => {
-       this.pokemonList = response;
-    }
-    )
+      this.pokemonList = response;
+    });
   }
-
 }
