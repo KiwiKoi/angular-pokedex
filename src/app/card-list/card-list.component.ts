f55c42810bf1e66605e40fbee9d149599520ddcc
Daniel <danielo.visage@gmail.com>
2022-08-27 13:25:14 +0200

    feat : update pokemon fetching and search

--- src/app/card-list/card-list.component.ts
+++ src/app/card-list/card-list.component.ts
@@ -10,8 +10,8 @@ import { FormControl, FormGroup } from '@angular/forms';
 })
 export class CardListComponent implements OnInit {
   pokemon!: Pokemon;
-  pokemonList!: Pokemon[];
-  pokemonDetail!: Pokemon[];
+  pokemonList: Pokemon[] = [];
+  searchResults: Pokemon[] = [];
   typeList!: any[];
   generations: any[] = [
     { name: 'generation 1', offset: 0, limit: 151 },
@@ -21,8 +21,10 @@ export class CardListComponent implements OnInit {
     { name: 'generation 5', offset: 493, limit: 156 },
     { name: 'generation 6', offset: 649, limit: 72 },
     { name: 'generation 7', offset: 721, limit: 88 },
-    { name: 'generation 8', offset: 809, limit: 96 }
+    { name: 'generation 8', offset: 809, limit: 96 },
   ];
+  showMessage: boolean = false;
+  message: string = 'No Pokémon Found';
   @Input() searchTerm!: string;
 
   form = new FormGroup({
@@ -37,23 +39,44 @@ export class CardListComponent implements OnInit {
     this.fetchAllTypes();
   }
 
-  filterPokemon() {
+  async filterPokemon() {
     let filterResults: Pokemon[] = [];
-    this.fetchAllPokemon();
-
-    for (let i = 0; i < this.pokemonList.length; i++) {
-      this.pokeDataService
-        .getPokemonDetail(this.pokemonList[i].name)
-        .subscribe((response: Pokemon) => {
-          if (
-            response.types.some(
-              (type) => type.type.name === this.form.value.type.name
-            )
-          ) {
-            filterResults.push(response);
-            this.pokemonList = filterResults;
-          }
-        });
+    // this.fetchAllPokemon();
+    console.log(filterResults);
+    if (filterResults.length > 0) {
+      filterResults = [];
+      for (let i = 0; i < this.pokemonList.length; i++) {
+        this.pokeDataService
+          .getPokemonDetail(this.pokemonList[i].name)
+          .subscribe((response: Pokemon) => {
+            if (
+              response.types.some(
+                (type) => type.type.name === this.form.value.type.name
+              )
+            ) {
+              filterResults.push(response);
+              this.pokemonList = filterResults;
+            }
+          });
+      }
+    } else {
+      for (let i = 0; i < this.pokemonList.length; i++) {
+        this.pokeDataService
+          .getPokemonDetail(this.pokemonList[i].name)
+          .subscribe((response: Pokemon) => {
+            if (
+              response.types.some(
+                (type) => type.type.name === this.form.value.type.name
+              )
+            ) {
+              filterResults.push(response);
+              this.pokemonList = filterResults;
+            } else {
+              this.showMessage = true;
+              this.pokemonList = [];
+            }
+          });
+      }
     }
   }
 
@@ -71,7 +94,13 @@ export class CardListComponent implements OnInit {
 
   fetchAllPokemon(offset?: number, limit?: number) {
     this.pokeDataService.getPokemonList().subscribe((response: Pokemon[]) => {
-      this.pokemonList = response;
+      response.forEach((pkmn) => {
+        this.pokeDataService
+          .getPokemonDetail(pkmn.name)
+          .subscribe((response: Pokemon) => {
+            this.pokemonList.push(response);
+          });
+      });
     });
   }
 
@@ -82,22 +111,16 @@ export class CardListComponent implements OnInit {
   }
 
   searchBar() {
-    let searchResults: Pokemon[] = [];
     if (this.searchTerm.length > 0) {
-      this.fetchAllPokemon();
-      for (let i = 0; i < this.pokemonList.length; i++) {
-        if (this.pokemonList[i].name.includes(this.searchTerm)) {
-          this.pokeDataService
-            .getPokemonDetail(this.pokemonList[i].name)
-            .subscribe((response: Pokemon) => {
-              searchResults.push(response);
-              this.pokemonList = searchResults;
-            });
-        } else {
-        }
-      }
+      this.pokemonList = this.pokemonList.filter((pokemon) => {
+        return pokemon.name.includes(this.searchTerm)
+      })
     } else {
+      this.searchResults = [];
+      this.pokemonList = [];
       this.fetchAllPokemon();
     }
   }
+
+  filterLoop() {}
 }
--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Pokemon } from '../models/pokemon';
-import {PokeDataService} from '../services/poke-data.service';
 
 @Component({
   selector: 'app-card',
@@ -8,20 +7,10 @@ import {PokeDataService} from '../services/poke-data.service';
   styleUrls: ['./card.component.scss']
 })
 export class CardComponent implements OnInit {
-@Input() pokemon!: any;
-
-pokemonDetail!: Pokemon;
-
-  constructor(private pokeDataService: PokeDataService){}
+@Input() pokemon!: Pokemon;
 
   ngOnInit(): void {
-    this.fetchPokemon();
   }
 
-fetchPokemon(){
-  this.pokeDataService.getPokemonDetail(this.pokemon.name).subscribe((response: Pokemon) => {
-    this.pokemonDetail = response;
-  })
-}
 
 }
