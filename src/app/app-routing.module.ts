50f2239195de9d09ceda20366a875f563024f97e
Daniel <danielo.visage@gmail.com>
2023-07-21 16:44:00 +0200

    fix : adjust routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,7 @@ import { CardListComponent } from './card-list/card-list.component';
 import { PokemonDetailComponent } from './pokemon-detail/pokemon-detail.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/pokedex', pathMatch: 'full' },
-  { path: 'pokedex', component: CardListComponent },
+  { path: '', component: CardListComponent },
   { path: 'detail/:id', component: PokemonDetailComponent },
 ];
 
